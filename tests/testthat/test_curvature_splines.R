52f4089a96fb1e3ad1514690db34ddf1c0964e36
mannfred <mannfredboehm@gmail.com>
2022-02-23 15:08:39 -0800

    adding a test for y=x^2

--- tests/testthat/test_curvature_splines.R
+++ tests/testthat/test_curvature_splines.R
@@ -34,6 +34,19 @@ test_that("total curvature of a unit circle is equal to radian distance", {
 
 })
 
+test_that("total curvature of x^2 between 0 and 1 is arctan of 2", {
+
+  # based on https://math.stackexchange.com/questions/3845048/finding-total-curvature-of-y-x2-x-in0-1
+  x <- seq(0, 1, 0.01)
+  y <- x^2
+  mdat <- matrix(c(x, y), nrow = 101, ncol = 2)
+
+  kspline <- curvature_spline(mdat, c(0, 1), 'ip')$Ktot %>% round(digits = 3)
+
+  expect_equal(kspline, round(atan2(2, 1), digits=3))
+
+})
+
 test_that("total curvature is equal between functions", {
 
   x <- seq(1, 10, 0.01)
